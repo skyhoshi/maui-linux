33058a5df229e2e172f68f5724ca7cede9d50454
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-31 14:41:51 +0200

    Added Shapes handler tests

--- src/Core/tests/DeviceTests/Handlers/ShapeView/ShapeViewHandlerTests.Android.cs
+++ src/Core/tests/DeviceTests/Handlers/ShapeView/ShapeViewHandlerTests.Android.cs
@@ -1,7 +1,20 @@
-﻿namespace Microsoft.Maui.DeviceTests
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.DeviceTests
 {
 	public partial class ShapeViewHandlerTests
 	{
-	
+		MauiShapeView GetNativeShapeView(ShapeViewHandler shapeViewHandler) =>
+			(MauiShapeView)shapeViewHandler.View;
+
+		Task ValidateNativeFill(IShapeView shapeView, Color color)
+		{
+			return InvokeOnMainThreadAsync(() =>
+			{
+				return GetNativeShapeView(CreateHandler(shapeView)).AssertContainsColor(color);
+			});
+		}
 	}
 }
--- src/Core/tests/DeviceTests/Handlers/ShapeView/ShapeViewHandlerTests.cs
+++ src/Core/tests/DeviceTests/Handlers/ShapeView/ShapeViewHandlerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.DeviceTests.Stubs;
+﻿using System.Threading.Tasks;
+using Microsoft.Maui.DeviceTests.Stubs;
 using Microsoft.Maui.Handlers;
 
 namespace Microsoft.Maui.DeviceTests
@@ -6,6 +7,94 @@ namespace Microsoft.Maui.DeviceTests
 	[Category(TestCategory.ShapeView)]
 	public partial class ShapeViewHandlerTests : HandlerTestBase<ShapeViewHandler, ShapeViewStub>
 	{
-	
+		[Fact(DisplayName = "Rectangle Initializes Correctly")]
+		public async Task RectangleInitializesCorrectly()
+		{
+			var rectangle = new ShapeViewStub()
+			{
+				Shape = new Graphics.Rectangle(),
+				Fill = Color.Red,
+				Height = 50,
+				Width = 100
+			};
+
+			await ValidateNativeFill(rectangle, Color.Red);
+		}
+
+		[Fact(DisplayName = "Ellipse Initializes Correctly")]
+		public async Task EllipseInitializesCorrectly()
+		{
+			var ellipse = new ShapeViewStub()
+			{
+				Shape = new Graphics.Ellipse(),
+				Fill = Color.Blue,
+				Height = 50,
+				Width = 100
+			};
+
+			await ValidateNativeFill(ellipse, Color.Blue);
+		}
+
+		[Fact(DisplayName = "Line Initializes Correctly")]
+		public async Task LineInitializesCorrectly()
+		{
+			var line = new ShapeViewStub()
+			{
+				Shape = new Graphics.Line { X1 = 0, Y1 = 0, X2 = 90, Y2 = 45 },
+				Stroke = Color.Purple,
+				StrokeThickness = 4,
+				Height = 50,
+				Width = 100
+			};
+
+			await ValidateNativeFill(line, Color.Purple);
+		}
+
+		[Fact(DisplayName = "Polyline Initializes Correctly")]
+		public async Task PolylineInitializesCorrectly()
+		{
+			var polyline = new ShapeViewStub()
+			{
+				Shape = new Graphics.Polyline { Points = new PointCollection() { new Point(10, 10), new Point(100, 50), new Point(50, 90) } },
+				Stroke = Color.Green,
+				StrokeThickness = 4,
+				Height = 50,
+				Width = 100
+			};
+
+			await ValidateNativeFill(polyline, Color.Green);
+		}
+
+		[Fact(DisplayName = "Polygon Initializes Correctly")]
+		public async Task PolygonInitializesCorrectly()
+		{
+			var polygon = new ShapeViewStub()
+			{
+				Shape = new Graphics.Polygon { Points = new PointCollection() { new Point(10, 10), new Point(100, 50), new Point(50, 90) } },
+				Fill = Color.Lime,
+				Stroke = Color.Black,
+				StrokeThickness = 4,
+				Height = 50,
+				Width = 100
+			};
+
+			await ValidateNativeFill(polygon, Color.Lime);
+		}
+
+		[Fact(DisplayName = "Path Initializes Correctly")]
+		public async Task PathInitializesCorrectly()
+		{
+			var path = new ShapeViewStub()
+			{
+				Shape = new Graphics.Path { Data = "M15.999996,0L31.999999,13.000001 15.999996,26.199999 0,13.000001z" },
+				Fill = Color.Coral,
+				Stroke = Color.Black,
+				StrokeThickness = 1,
+				Height = 50,
+				Width = 50
+			};
+
+			await ValidateNativeFill(path, Color.Coral);
+		}
 	}
 }
