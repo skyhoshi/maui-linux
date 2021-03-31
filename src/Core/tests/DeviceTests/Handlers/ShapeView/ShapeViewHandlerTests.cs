7cb4ecea64295fe88e0ec1a7bcc8d3eabb95e79a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-03-31 14:08:58 +0200

    Include ShapeViewStub

---
+++ src/Core/tests/DeviceTests/Handlers/ShapeView/ShapeViewHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	[Category(TestCategory.ShapeView)]
+	public partial class ShapeViewHandlerTests : HandlerTestBase<ShapeViewHandler, ShapeViewStub>
+	{
+	
+	}
+}
---
+++ src/Core/tests/DeviceTests/Stubs/ShapeViewStub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace Microsoft.Maui.DeviceTests.Stubs
+{
+	public partial class ShapeViewStub : StubBase, IShapeView
+	{
+		public Shape Shape { get; set; }
+
+		public Color Fill { get; set; }
+
+		public Color Stroke { get; set; }
+
+		public double StrokeThickness { get; set; }
+
+		public DoubleCollection StrokeDashArray { get; set; }
+
+		public double StrokeDashOffset { get; set; }
+
+		public PenLineCap StrokeLineCap { get; set; }
+
+		public PenLineJoin StrokeLineJoin { get; set; }
+
+		public double StrokeMiterLimit { get; set; }
+
+		public Stretch Stretch { get; set; }
+	}
+}
