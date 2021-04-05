259e2682b3b12b946da535fa8c588f14cdffd934
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-05 16:34:17 +0200

    Updated comments

--- src/Core/src/Core/IBorder.cs
+++ src/Core/src/Core/IBorder.cs
@@ -1,17 +1,17 @@
 ﻿namespace Microsoft.Maui
 {
 	/// <summary>
-	/// 
+	/// Provides functionality to set the view borders.
 	/// </summary>
 	public interface IBorder
 	{
 		/// <summary>
-		/// 
+		/// Gets the the color of a view's border.
 		/// </summary>
 		Color BorderColor { get; }
 
 		/// <summary>
-		/// 
+		/// Gets the the width of a view's border.
 		/// </summary>
 		double BorderWidth { get; }
 	}
