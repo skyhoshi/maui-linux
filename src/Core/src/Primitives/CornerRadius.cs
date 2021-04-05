dc3b67bf00975e2377ce38de5bfec6e2c0a45a1b
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-04-05 16:54:24 +0200

    Added nullability check to CornerRadius

--- src/Core/src/Primitives/CornerRadius.cs
+++ src/Core/src/Primitives/CornerRadius.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Maui
 			return TopLeft == other.TopLeft && TopRight == other.TopRight && BottomLeft == other.BottomLeft && BottomRight == other.BottomRight;
 		}
 
-		public override bool Equals(object obj)
+		public override bool Equals(object? obj)
 		{
 			if (ReferenceEquals(null, obj))
 				return false;
